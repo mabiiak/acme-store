95b14d99219c1cb79b90a315b7c0a8e795ccfa2f
mabiak <mabiane.polniak@gmail.com>
2022-06-03 01:04:02 -0300

    fix: provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
 import { HashRouter, Routes, Route } from 'react-router-dom';
-import Provider from './context/Provider';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
 import ProductDetails from './pages/ProductDetails';
 
 function App() {
   return (
-    <Provider>
-      <HashRouter>
-        <Routes>
-          <Route exact path="/" element={ <Home /> } />
-          <Route exact path="/cart" element={ <Cart /> } />
-          <Route path="/product" element={ <ProductDetails /> } />
-        </Routes>
-      </HashRouter>
-    </Provider>
+    <HashRouter>
+      <Routes>
+        <Route exact path="/" element={ <Home /> } />
+        <Route exact path="/cart" element={ <Cart /> } />
+        <Route path="/product" element={ <ProductDetails /> } />
+      </Routes>
+    </HashRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Provider from './context/Provider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
+  <Provider>  
+    <React.StrictMode>
+      <App />
   </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
