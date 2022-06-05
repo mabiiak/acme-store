6f1a236346aff1826e59c406b71356260b608607
mabiak <mabiane.polniak@gmail.com>
2022-06-05 13:38:41 -0300

    style: header

---
+++ src/styles/PrincipalHeader.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const PrincipalHeader = styled.header`
+  align-items: center;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  margin-left: 19%;
+  margin-right:19%;
+  padding: 5px;
+  text-align: center;
+  
+  div > a > img{
+    height: 50px;
+    margin-left: 15px;
+    padding: 10px;
+    width: 50px;
+  }
+`
