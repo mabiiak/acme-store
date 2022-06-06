3d6cce9fe396ed2cdea44b2faa10075b817464ca
mabiak <mabiane.polniak@gmail.com>
2022-06-05 22:50:55 -0300

    refactor: reuse component ResumeCard in FilterName

---
+++ src/components/Filter/FilterName.jsx
@@ -0,0 +1,21 @@
+import React, { useContext, useEffect } from 'react';
+import { Context } from '../../context/Provider'
+import ResumeCard from '../ResumeCard';
+
+function FilterName() {
+  const { listProducts, filterName, filtredProducts, setFiltredProducts } = useContext(Context);
+
+  useEffect(() => {
+    if(filterName !== '') {
+      const searched = listProducts.filter((item) => item.name.toLowerCase().includes(filterName.toLowerCase()));
+
+      if(searched.length !== 0) { setFiltredProducts(searched)}
+    }
+  }, [filterName])
+
+  return(
+    <ResumeCard list={ filtredProducts } />
+  )
+}
+
+export default FilterName;
