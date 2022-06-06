06fd7b7956a815fce4106c854c9358f0848741b9
mabiak <mabiane.polniak@gmail.com>
2022-06-05 22:49:34 -0300

    feat: create component resume card

---
+++ src/components/ResumeCard.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+import { SectionProducts } from "../styles/Card";
+import heart from '../images/heart.png';
+import cart from '../images/cart.png';
+
+class ResumeCard extends React.Component {
+  constructor() {
+    super()
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick({ target }) {
+    const { name, id } = target;
+
+    const selectedItem = this.props.list.find((item) => item.name === id);
+    const initialStorage = JSON.parse(localStorage.getItem(name));
+
+    if(initialStorage === null) {
+      localStorage.setItem(name, JSON.stringify([{...selectedItem} ]));
+    } else {
+      localStorage.setItem(name, JSON.stringify([...initialStorage, {...selectedItem}]));
+    }
+  }
+
+  render() {
+    const { list } = this.props;
+
+    return (
+      <SectionProducts>
+        {
+          list.map((item) => (
+            <div key={ item.name } className='card'>
+              <Link
+                to={`/product/${item.name.toLowerCase().split(' ').join('_')}`}
+              >
+                <img src={item.url} alt='imagem do produto'/>
+                <div>
+                <h4>{ item.name }</h4>
+                <p>R$00,00</p>
+                </div>
+              </Link>
+              <div className='buttons'>
+                <img
+                  src={ heart }
+                  alt='heart icon'
+                  className='button'
+                  name='heart'
+                  onClick={ this.handleClick }
+                  id={ item.name }
+                  />
+                <img
+                  src={ cart }
+                  alt='cart icon'
+                  className='button'
+                  name='cart'
+                  onClick={ this.handleClick }
+                  id={ item.name }
+                />
+              </div>
+            </div>
+          ))
+        }
+      </SectionProducts>
+    )
+  }
+}
+
+export default ResumeCard;
