52830fbe18ff2f5df684a91bb1ca7ef31bb8774a
mabiak <mabiane.polniak@gmail.com>
2022-06-05 22:51:14 -0300

    refactor: reuse component 'ResumeCard' in AllCards

--- src/components/AllCards.jsx
+++ src/components/AllCards.jsx
@@ -1,62 +1,12 @@
 import React, { useContext } from 'react';
 import { Context } from '../context/Provider'
-import { Link } from 'react-router-dom';
-import { SectionProducts } from '../styles/Card'; 
-import heart from '../images/heart.png';
-import cart from '../images/cart.png';
+import ResumeCard from './ResumeCard';
 
 function AllCards() {
   const { listProducts } = useContext(Context);
 
-  function handleClick({ target }) {
-    const { name, id } = target;
-    const selectedItem = listProducts.find((item) => item.name === id);
-
-    const initialStorage = JSON.parse(localStorage.getItem(name));
-
-    if(initialStorage === null) {
-      localStorage.setItem(name, JSON.stringify([{...selectedItem} ]));
-    } else {
-      localStorage.setItem(name, JSON.stringify([...initialStorage, {...selectedItem}]));
-    }
-  }
-
   return(
-    <SectionProducts>
-    {
-      listProducts.map((item) => (
-        <div key={ item.name } className='card'
-        >
-          <Link to={`/product/${item.name.toLowerCase().split(' ').join('_')}`} >
-            <img src={item.url} alt='imagem do produto'/>
-            <div>
-            <h4>{ item.name }</h4>
-            <p>R$00,00</p>
-            </div>
-          </Link>
-          <div className='buttons'>
-            <img
-              src={ heart }
-              alt='heart icon'
-              className='button'
-              name='heart'
-              onClick={ handleClick }
-              id={ item.name }
-            />
-            <img
-              src={ cart }
-              alt='cart icon'
-              className='button'
-              name='cart'
-              onClick={ handleClick }
-              id={ item.name }
-            />
-          </div>
-        </div>
-
-      ))
-    }
-    </SectionProducts>
+    <ResumeCard list={ listProducts } />
   )
 }
 
