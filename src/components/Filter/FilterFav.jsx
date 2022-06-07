a329ac03f729a47329e9566ab1832cd433a525f1
mabiak <mabiane.polniak@gmail.com>
2022-06-07 15:38:18 -0300

    feat:  create logic remove favorite

--- src/components/Filter/FilterFav.jsx
+++ src/components/Filter/FilterFav.jsx
@@ -2,7 +2,11 @@ import React, { useEffect, useState, useContext } from 'react';
 import { Context } from '../../context/Provider';
 import { getLocalStorage } from '../../services/localStorage';
 import { Title } from '../../styles/Headers';
+import { Link } from 'react-router-dom';
 import ResumeCard from '../ResumeCard';
+import { SectionProducts } from '../../styles/Card';
+import heart  from '../../images/heart.png';
+import cart  from '../../images/cart.png'
 
 function FilterFavCards() {
   const { price } = useContext(Context);
@@ -13,18 +17,80 @@ function FilterFavCards() {
     setSearch(searched)
   }, [])
 
+  function handleClick({ target }) {
+    const { name, id } = target;
+
+    const selectedItem = researched.find((item) => item.name === id);
+    const initialStorage = JSON.parse(localStorage.getItem(name));
+    let checkExist = false;
+
+    if(initialStorage !== null) {
+      checkExist = initialStorage.some((item) => item.name === id);
+    }
+  
+    if(initialStorage === null) {
+      localStorage.setItem(name, JSON.stringify([{...selectedItem} ]));
+
+    } else if (initialStorage !== null && checkExist === false) {
+      localStorage.setItem(name, JSON.stringify([...initialStorage, {...selectedItem}]));
+
+    } else if (name === 'heart' && initialStorage !== null && checkExist === true) {
+      const removeItem = initialStorage.filter((item) => item.name !== id)
+      localStorage.setItem(name, JSON.stringify([...removeItem]));
+      setSearch(removeItem);
+    } else if (name === 'cart' && initialStorage !== null && checkExist === true) {
+      const removeItem = initialStorage.filter((item) => item.name !== id)
+      localStorage.setItem(name, JSON.stringify([...removeItem]));
+    }
+
+  };
   
   return(
     <div>
-      <Title>
-        <h3>
-          Favoritos
-        </h3>
-      </Title>
+      <Title><h3> Favoritos </h3></Title>
       {
         researched !== null
-      ? <ResumeCard list={ researched } page='fav' />
-      : <p>Não há favoritos</p>}
+        && (
+          <SectionProducts>
+            {
+              researched.map((item, index) => (
+                <div key={ item.name } className='card'>
+                  <Link
+                    to={`/product/${item.name.toLowerCase().split(' ').join('_')}`}
+                  >
+                    <img src={item.url} alt='imagem do produto'/>
+                    <div>
+                      <h4>{ item.name }</h4>
+                      {
+                        researched.length === 10
+                        && <p> R$ { researched[index] } </p>
+                      }
+                    </div>
+                  </Link>
+                  <div className='buttons'>
+                    <img
+                      src={ heart }
+                      alt='heart icon'
+                      className='button'
+                      name='heart'
+                      onClick={ handleClick }
+                      id={ item.name }
+                    />
+                    <img
+                      src={ cart }
+                      alt='cart icon'
+                      className='button'
+                      name='cart'
+                      onClick={ handleClick }
+                      id={ item.name }
+                    />
+                  </div>
+                </div>
+              ))
+            }
+          </SectionProducts>
+        )}
+      { researched.length === 0 && <p>Não há favoritos</p> }
     </div>
   )
 }
