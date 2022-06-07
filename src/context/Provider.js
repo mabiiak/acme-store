4f0c36176c4008bd92ded54cc72e17a899ded218
mabiak <mabiane.polniak@gmail.com>
2022-06-06 21:16:33 -0300

    refactor: recreate useEffect functions in provider

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,4 +1,6 @@
 import React, { createContext, useState } from 'react';
+import getPrice from '../services/calcPrice';
+import generateName from '../services/utils';
 
 export const Context = createContext();
 
@@ -11,8 +13,48 @@ function Provider({ children }) {
   const [filterFavorite, setFilterFavorite] = useState(false);
   const [filtredProducts, setFiltredProducts] = useState([]);
 
+  const [price, setPrice] = useState ([]);
+
   let listProducts = [];
 
+  function createPicturesAndNames() {
+    setAllPictures([]);
+
+    for(let index = 0; index < 10; index += 1) {
+      fetch('https://picsum.photos/200').then((response) => {
+        return setAllPictures((oldPictures) => [...oldPictures, response.url]);
+      }).catch(function(err) { console.log('Fetch Error', err) }); 
+    }
+
+    setNames(generateName());
+    return namesProducts
+  }
+
+  function createListDescriptions() {
+    namesProducts.map((name) => {
+      let word = name.split(' ')[0];
+      if (word === 'Bafômetro' || word === 'Estátua') return word = 'Macarrão';
+      const wordRegex = word.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+      // Referencia Regex ---> https://www.horadecodar.com.br/2020/12/07/como-remover-acentos-em-javascript/
+
+      const URL = `https://significado.herokuapp.com/v2/${wordRegex}`  
+      return fetch(URL).then(response => response.json())
+        .then(response => {
+          return setDescribe((prevSig) => [...prevSig, response[0].meanings[0]]);
+      }).catch(err => console.error(err));
+    });
+
+    return describe;
+  }
+
+  function createListPrices() {
+    for(let index = 0; index < 10; index +=1) {
+      let value = getPrice(namesProducts[index], describe[index]);
+
+      setPrice((oldValues) => [...oldValues, value]);
+    }
+  }
+  
   allPictures.map((picture, index) => {
     return listProducts = [
       ...listProducts,
@@ -22,7 +64,7 @@ function Provider({ children }) {
         url: picture,
         id: namesProducts[index].split(' ')[0],
         description: describe[index],
-        // price: getPrice(namesProducts[index], describe[index])      
+        price: price[index],   
       }
     ]
   });
@@ -40,7 +82,12 @@ function Provider({ children }) {
     filterFavorite,
     setFilterFavorite,
     filtredProducts,
-    setFiltredProducts
+    setFiltredProducts,
+    price, setPrice,
+
+    createPicturesAndNames,
+    createListDescriptions,
+    createListPrices,
   };
 
   return (
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,57 +1,27 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import { Context } from '../context/Provider'
 import Header from '../components/Header';
 import AllCards from '../components/AllCards';
-import generateName from '../services/utils';
 import InputFilter from '../components/Filter/InputFilter';
 import FilterName from '../components/Filter/FilterName';
 import FilterFavCards from '../components/Filter/FilterFav';
 
 function Home() {
   const {
-    setAllPictures,
-    setNames,
+    createPicturesAndNames,
+    createListDescriptions,
     listProducts,
-    setDescribe,
-    describe,
     filterName,
     filterFavorite,
   } = useContext(Context);
 
+  const [picturesAndNames, setPicturesAndNames] = useState({});
+
   useEffect(() => {
-    if(listProducts.length === 0) {
-      setAllPictures([]);
-      for(let index = 0; index < 10; index += 1) {
-        fetch('https://picsum.photos/200')
-          .then((response) => {
-            return setAllPictures((oldPictures) => [...oldPictures, response.url]);
-          })
-          .catch(function(err) { console.log('Fetch Error', err) });
-        }
-      setNames(generateName());
-    }
+    if(listProducts.length === 0) return  setPicturesAndNames(createPicturesAndNames());
   }, []);
 
-  useEffect(() => {
-    if (describe.length === 0) {
-      for(let index = 0; index < listProducts.length; index += 1) {
-        setDescribe([]);
-
-        const word = listProducts[index].id;
-        const wordRegex = word.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
-        // Referencia Regex ---> https://www.horadecodar.com.br/2020/12/07/como-remover-acentos-em-javascript/
-
-        const URL = `https://significado.herokuapp.com/v2/${wordRegex}`
-
-        fetch(URL)
-          .then(response => response.json())
-          .then(response => {
-            return setDescribe((prevSig) => [...prevSig, response]);
-          })
-          .catch(err => console.error(err));
-      };
-    }
-  },[listProducts.length === 10]);
+  useEffect(() => { createListDescriptions() }, [picturesAndNames, setPicturesAndNames]);
 
   return(
     <div className='home_color'>
